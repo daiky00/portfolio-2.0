7f73461424b294a18fc12814132fc2ca4c299e61
Francisco Pena <project.01@hotmail.com>
2018-07-21 17:58:45 -0500

    created a list component

---
+++ src/components/List/ListItem.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const ListItem = styled.li`
+	display: ${(props) => (props.inline ? 'inline' : 'block')};
+	margin-right: ${(props) => (props.inline ? '1rem' : '0')};
+	font-size: 130%;
+`;
+
+export default ListItem;
