85568c1f289a74e6ec6cd41e4b901af649d4128e
Francisco Pena <project.01@hotmail.com>
2018-07-21 00:34:07 -0500

    added image component

---
+++ src/components/img.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Img = styled.img`
+	width: ${(props) => props.sizeX};
+	height: ${(props) => props.sizeY};
+	max-width: ${(props) => props.max};
+`;
+
+export default Img;
