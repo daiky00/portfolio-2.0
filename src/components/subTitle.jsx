cb16a597b60ce8734a71d43d50b9ed6966308143
Francisco Pena <project.01@hotmail.com>
2018-07-21 00:33:53 -0500

    made header changes and added both subtitle and title

---
+++ src/components/subTitle.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const SubTitle = styled.h2`
+	color: #4ab19a;
+	font-size: 180%;
+	font-weight: 100;
+`;
+
+export default SubTitle;
---
+++ src/components/title.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+	font-family: grand hotel, cursive;
+	color: #4ab19a;
+	font-size: 600%;
+	font-weight: 100;
+	margin: 6rem 0 0;
+	line-height: 0.8;
+`;
+
+export default Title;
