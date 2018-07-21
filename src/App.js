2a987a88f45199df83feeaa933f90dc8605b7cb3
Francisco Pena <project.01@hotmail.com>
2018-07-21 00:33:07 -0500

    added components to home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import Me from './me.svg';
+import Land from './land.svg';
 import Header from './components/header';
-import Avatar from './components/avatar';
+import Img from './components/img';
+import Title from './components/title';
+import SubTitle from './components/subTitle';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
 class App extends Component {
@@ -10,9 +13,28 @@ class App extends Component {
 			<React.Fragment>
 				<Header>
 					<Grid>
-						<Avatar src={Me} size="102px" />
+						<Img src={Me} sizeX="102px" />
+						<Row center="xs">
+							<Col xs={12}>
+								<Title>I'm Francisco</Title>
+							</Col>
+							<SubTitle>Frontend Developer/Designer UI/UX</SubTitle>
+						</Row>
+						<Row bottom="xs" center="xs">
+							<Col xs={12}>
+								<Img src={Land} sizeX="100%" sizeY="100%" max="800px" />
+							</Col>
+						</Row>
 					</Grid>
 				</Header>
+				<Grid>
+					<Row center="xs">
+						<Img src={Me} sizeX="152px" sizeY="200px" />
+						<Col xs={12}>
+							<SubTitle>Hi.</SubTitle>
+						</Col>
+					</Row>
+				</Grid>
 			</React.Fragment>
 		);
 	}
