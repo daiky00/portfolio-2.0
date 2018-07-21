9c0eb91c608de6c9dae130836b03ed5f8224e449
Francisco Pena <project.01@hotmail.com>
2018-07-21 18:40:47 -0500

    remove global styling for links

--- src/components/List/ListItem.jsx
+++ src/components/List/ListItem.jsx
@@ -4,6 +4,14 @@ const ListItem = styled.li`
 	display: ${(props) => (props.inline ? 'inline' : 'block')};
 	margin-left: ${(props) => (props.inline ? '1rem' : '0')};
 	font-size: 130%;
+
+	a {
+		color: #4ab19a;
+		text-decoration: none;
+		&:hover {
+			color: #1e836c;
+		}
+	}
 `;
 
 export default ListItem;
