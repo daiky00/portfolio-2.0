5c189d8dc772230eb018a5de5178e0e161bf354e
Francisco Pena <project.01@hotmail.com>
2018-07-21 20:25:59 -0500

    added a pure html css code editor animation

---
+++ src/components/Code/Code.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import './Code.css';
+
+class Code extends Component {
+	state = {};
+	render() {
+		return (
+			<div className="sublime-text">
+				<div className="screen">
+					<div className="left-bar" />
+					<div className="top-bar">
+						<div className="buttons" />
+					</div>
+					<div className="code">
+						<div className="line line1">
+							<p className="code-item green" /> <p className="code-item purple" />
+						</div>
+						<div className="line line2">
+							<p className="code-item blue" /> <p className="code-item yellow" />{' '}
+							<p className="code-item green" />
+						</div>
+						<div className="line line3">
+							<p className="code-item transparent" /> <p className="code-item orange" />{' '}
+							<p className="code-item green" />
+							<p className="code-item purple" />
+						</div>
+						<div className="line line4">
+							<p className="code-item transparent" /> <p className="code-item transparent" />
+							<p className="code-item yellow" /> <p className="code-item blue" />{' '}
+							<p className="code-item green"> </p>
+						</div>
+						<div className="line line5">
+							<p className="code-item transparent" /> <p className="code-item transparent" />
+							<p className="code-item purple" /> <p className="code-item orange" />{' '}
+							<p className="code-item yellow" />
+						</div>
+						<div className="line line6">
+							<p className="code-item transparent" /> <p className="code-item green" />{' '}
+							<p className="code-item blue" />
+							<p className="code-item purple"> </p>
+						</div>
+						<div className="line line7">
+							<p className="code-item transparent" /> <p className="code-item yellow" />
+							<p className="code-item purple" /> <p className="code-item green" />
+						</div>
+						<div className="line line8">
+							<p className="code-item transparent" /> <p className="code-item transparent" />
+							<p className="code-item orange" /> <p className="code-item green" />{' '}
+							<p className="code-item purple"> </p>
+						</div>
+						<div className="line line9">
+							<p className="code-item transparent" /> <p className="code-item blue" />{' '}
+							<p className="code-item yellow" />
+							<p className="code-item cursor" />
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Code;
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Title from '../components/Title';
 import SubTitle from '../components/SubTitle';
 import Section from '../components/Section';
+import Code from '../components/Code/Code';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
 const Work = () => (
@@ -11,8 +12,13 @@ const Work = () => (
 				<Row center="xs">
 					<Col xs={12}>
 						<Title>My work</Title>
+						<SubTitle>My portfolio made with coffee</SubTitle>
 					</Col>
-					<SubTitle>My portfolio made with coffee</SubTitle>
+				</Row>
+				<Row bottom="xs" center="xs">
+					<Col xs={12}>
+						<Code />
+					</Col>
 				</Row>
 			</Grid>
 		</Section>
