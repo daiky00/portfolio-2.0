5ff2754240c19c342bb05edb3affbc077d409179
Francisco Pena <project.01@hotmail.com>
2019-02-06 01:03:01 -0600

    fixing fonts for portfolio

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Paragraph = styled.p`
 	color: ${(props) => props.color} !important;
 	font-size: 140%;
-	font-weight: 100;
+	font-weight: 300;
 	color: #545454;
 `;
 
--- src/components/SubTitle.js
+++ src/components/SubTitle.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const SubTitle = styled.h2`
 	color: ${(props) => props.color};
 	font-size: 180%;
-	font-weight: 100;
+	font-weight: 400;
 `;
 
 export default SubTitle;
