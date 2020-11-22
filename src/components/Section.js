616e0d00a40f1dca07f9018859f64377f7b74fcb
Francisco Pena <project.01@hotmail.com>
2020-11-21 22:50:07 -0600

    fix section

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const Section = styled.section`
-	padding: 1rem 0 3rem;
+	padding: 1rem 0 0;
 	background-color: ${(props) => props.backgroundColor};
 `;
 
